5c9e55857990e4cbd0c197491d32d7296d1d9787
Grigory Ivanov <givanov@web3tech.ru>
2021-07-07 16:34:37 +0300

    Clean up
    react-app start
    
    Simple Reac app
    
    Added page like docs

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { Signer } from '@waves/signer';
+import { ProviderWeb } from '@waves.exchange/provider-web';
+import { ProviderCloud } from '@waves.exchange/provider-cloud';
+
+const signerWeb = new Signer({
+  NODE_URL: 'https://nodes-testnet.wavesnodes.com'
+});
+signerWeb.setProvider(new ProviderWeb('https://testnet.waves.exchange/signer/'));
+
+const signerCloud = new Signer({
+  NODE_URL: 'https://nodes-testnet.wavesnodes.com'
+});
+signerCloud.setProvider(new ProviderCloud('https://testnet.waves.exchange/signer-cloud/'));
+
+function testlogin(signer) {
+  return signer.login();
+};
+
+function testsend(signer) {
+  return signer
+    .transfer({amount: 100, recipient: 'alias:T:merry'}) // Transfer 1 WAVES to alias merry
+    .broadcast(); 
+};
+
+function App() {
+  return (
+    <div className="TestApp">
+    <div>
+    <h3>Provider-Web</h3>
+    <div>https://testnet.waves.exchange/signer</div>
+      <div>
+        <button onClick={() => testlogin(signerWeb)}>LOGIN</button>
+      </div>
+      <div>
+        <button onClick={() => testsend(signerWeb)}>TRANSFER</button>
+      </div>
+    </div>
+    <div>
+    <h3>Provider-Cloud</h3>
+    <div>https://testnet.waves.exchange/signer-cloud</div>
+      <div>
+        <button onClick={() => testlogin(signerCloud)}>LOGIN</button>
+      </div>
+      <div>
+        <button onClick={() => testsend(signerCloud)}>TRANSFER</button>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;

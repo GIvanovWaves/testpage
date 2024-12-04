0d637dbd44bc7afc9091c2d2be4acfb126fb7000
Grigory Ivanov <36446723+GIvanovWaves@users.noreply.github.com>
2024-12-04 11:12:00 +0300

    build fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
+    mode: "production",
     entry: "./src/index.js",
     output: {
-        filename: "index.js",
         path: path.join(process.cwd(), 'docs'),
     },
 };
